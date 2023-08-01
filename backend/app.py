cebe40c1e8560014ec056d5009b41ccf0b70bf04
renevan10 <renevan10@gmail.com>
2023-08-01 15:23:18 -0700

    Fix db table for unique n and add API to get sequence

--- backend/app.py
+++ backend/app.py
@@ -13,7 +13,7 @@ CORS(app)
 
 class Sequence(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    n = db.Column(db.Integer, nullable=False)
+    n = db.Column(db.Integer, nullable=False, unique=True)
     term = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
@@ -22,3 +22,38 @@ class Sequence(db.Model):
     def __init__(self, n, term):
         self.n = n
         self.term = term
+
+
+@app.route('/getSequence/<n>', methods=['GET'])
+def get_entry(n):
+    n = int(n)
+    res = []
+    for i in range(1, n+1):
+        exists = bool(Sequence.query.filter_by(n=i).first())
+        if exists == False:
+            if i == 1 or i == 2:
+                sequence = Sequence(i, 1)
+                db.session.add(sequence)
+                db.session.commit()
+                res.append(format_sequence(sequence))
+            else:
+                num1 = Sequence.query.filter_by(n=i-2).one()
+                num2 = Sequence.query.filter_by(n=i-1).one()
+                sequence = Sequence(i, num1.term+num2.term)
+                db.session.add(sequence)
+                db.session.commit()
+                res.append(format_sequence(sequence))
+        else:
+            num = Sequence.query.filter_by(n=i).one()
+            res.append(format_sequence(num))
+    return {
+        "sequence": res
+    }
+
+
+def format_sequence(sequence):
+    return {
+        "id": sequence.id,
+        "n": sequence.n,
+        "term": sequence.term,
+    }
